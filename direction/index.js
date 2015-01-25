6418349292a18638eb192143b2d45712266d6df5
Stefan Holzmüller <stefanholzmueller@github>
2015-01-25 13:24:20 +0100

    Januarprojekt test

--- direction/index.js
+++ direction/index.js
@@ -6,10 +6,11 @@ module.controller("directionCtrl", function ($scope, $timeout) {
 
             var currentPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
             map.setCenter(currentPosition)
-            map.markers.arrow.position = currentPosition;
+            map.markers.arrow.setPosition(currentPosition);
 
             if (position.coords.heading) {
-                map.markers.arrow.icon.rotation = position.coords.heading;
+                map.markers.arrow.icon.rotation = 45;
+                map.markers.arrow.setIcon(map.markers.arrow.icon);
             }
 
         }, function (error) {
