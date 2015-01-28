ee3e0c79cb0effcca0adab043edcfed7a361c120
Stefan Holzmüller <stefanholzmueller@github>
2015-01-28 07:33:15 +0100

    Januarprojekt: direction2 with enableHighAccuracy

---
+++ direction2/index.js
@@ -0,0 +1,21 @@
+var module = angular.module("direction", ['ngMap']);
+
+module.controller("directionCtrl", function ($scope, $timeout) {
+    $scope.$on('mapInitialized', function (event, map) {
+        navigator.geolocation.watchPosition(function (position) {
+
+            var currentPosition = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+            map.setCenter(currentPosition)
+            map.markers.arrow.setPosition(currentPosition);
+
+            if (position.coords.heading) {
+                map.markers.arrow.icon.rotation = position.coords.heading;
+                map.markers.arrow.setIcon(map.markers.arrow.icon);
+            }
+
+        }, function (error) {
+            alert(error);
+        }, {timeout: 5000, enableHighAccuracy: true});
+    });
+
+});
